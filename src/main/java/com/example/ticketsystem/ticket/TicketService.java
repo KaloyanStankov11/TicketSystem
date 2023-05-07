1761c952b0bea77432f2a8906a23a798909c7b42
KaloyanStankov11 <kaloqn.t@mail.bg>
2023-05-07 20:07:37 +0300

    calculate ticket price

--- src/main/java/com/example/ticketsystem/ticket/TicketService.java
+++ src/main/java/com/example/ticketsystem/ticket/TicketService.java
@@ -1,7 +1,20 @@
 package com.example.ticketsystem.ticket;
 
+import com.example.ticketsystem.card.DiscountCard;
+import com.example.ticketsystem.route.Route;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class TicketService {
+
+    public BigDecimal calculateTicketPrice(Route route, DiscountCard discountCard){
+        BigDecimal discountPercent = BigDecimal.ZERO;
+        BigDecimal defaultPrice = route.getTicketPrice();
+        if (discountCard != null){
+            discountPercent = discountCard.getDiscountPercent();
+        }
+        return defaultPrice.subtract(defaultPrice.multiply(discountPercent));
+    }
 }
