67eb928d370583b6e33ee70744271454cafe520e
KaloyanStankov11 <kaloqn.t@mail.bg>
2023-05-08 00:35:23 +0300

    create reservation

--- src/main/java/com/example/ticketsystem/reservation/Reservation.java
+++ src/main/java/com/example/ticketsystem/reservation/Reservation.java
@@ -16,4 +16,44 @@ public class Reservation extends _BaseEntity {
     private LocalDateTime timeCreated;
     private LocalDateTime validTo;
     private String status;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getValidTo() {
+        return validTo;
+    }
+
+    public void setValidTo(LocalDateTime validTo) {
+        this.validTo = validTo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/example/ticketsystem/reservation/ReservationService.java
+++ src/main/java/com/example/ticketsystem/reservation/ReservationService.java
@@ -1,8 +1,22 @@
 package com.example.ticketsystem.reservation;
 
 import com.example.ticketsystem.Base._BaseService;
+import com.example.ticketsystem.ticket.Ticket;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ReservationService extends _BaseService {
+
+    public void createReservation(Long userId, List<Ticket> tickets){
+        LocalDateTime currentTime = LocalDateTime.now();
+        Reservation reservation = new Reservation();
+        reservation.setStatus(ReservationStatus.ACTIVE.name());
+        reservation.setTickets(tickets);
+        reservation.setTimeCreated(currentTime);
+        reservation.setUserId(userId);
+        em.persist(reservation);
+    }
 }
