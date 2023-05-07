65592d81114a8cd5d8105d3ff3e59b241ae99b44
KaloyanStankov11 <kaloqn.t@mail.bg>
2023-05-07 20:08:48 +0300

    getter and setters

--- src/main/java/com/example/ticketsystem/card/DiscountCard.java
+++ src/main/java/com/example/ticketsystem/card/DiscountCard.java
@@ -9,4 +9,20 @@ import java.math.BigDecimal;
 public class DiscountCard extends _BaseEntity {
     private String cardType;
     private BigDecimal discountPercent;
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public BigDecimal getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(BigDecimal discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }
--- src/main/java/com/example/ticketsystem/route/Route.java
+++ src/main/java/com/example/ticketsystem/route/Route.java
@@ -12,4 +12,36 @@ public class Route extends _BaseEntity {
     private String endCity;
     private LocalDateTime departTime;
     private BigDecimal ticketPrice;
+
+    public String getStartCity() {
+        return startCity;
+    }
+
+    public void setStartCity(String startCity) {
+        this.startCity = startCity;
+    }
+
+    public String getEndCity() {
+        return endCity;
+    }
+
+    public void setEndCity(String endCity) {
+        this.endCity = endCity;
+    }
+
+    public LocalDateTime getDepartTime() {
+        return departTime;
+    }
+
+    public void setDepartTime(LocalDateTime departTime) {
+        this.departTime = departTime;
+    }
+
+    public BigDecimal getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(BigDecimal ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
 }
