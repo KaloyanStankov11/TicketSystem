2f5e874c802e1bc0f4e09b57b783746f5c448e41
KaloyanStankov11 <kaloqn.t@mail.bg>
2023-05-08 09:27:59 +0300

    find route

--- src/main/java/com/example/ticketsystem/route/RouteService.java
+++ src/main/java/com/example/ticketsystem/route/RouteService.java
@@ -3,6 +3,15 @@ package com.example.ticketsystem.route;
 import com.example.ticketsystem.Base._BaseService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class RouteService extends _BaseService {
+
+    public Route findRoute(String destinationCity, LocalDateTime departureTime){
+        return em.createQuery("SELECT route FROM Route route where route.endCity = ?1 AND route.departTime = ?2", Route.class)
+                .setParameter(1, destinationCity)
+                .setParameter(2, departureTime)
+                .getSingleResult();
+    }
 }
